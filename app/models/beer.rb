9281bc42310bec67e74d3960ea962b5ed74527b3
wadror <wadror@wadror-VirtualBox.(none)>
2015-01-21 19:48:11 +0200

    5 done

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,8 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    ratings.map{|rating| rating.score}.inject{|sum, n| sum + n}/(ratings.length * 1.0)
+  end
 end
