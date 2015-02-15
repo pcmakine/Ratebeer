213c811d4e6af3993a862eec40ecd3a497d5cb68
Petri <wadror@wadror-VirtualBox.(none)>
2015-02-15 12:49:26 +0200

    styles table created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128125754) do
+ActiveRecord::Schema.define(version: 20150215104153) do
 
   create_table "beer_clubs", force: true do |t|
     t.string   "name"
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 20150128125754) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "styles", force: true do |t|
+    t.text     "description"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
